fcf1738033e7eb8ca73bcd2852ec2f7d3f442109
mhelmkampf <martin.helmkampf@leibniz-zmt.de>
2026-05-29 13:56:34 +0200

    session 05, update 01

--- code/06_gdiv.sh
+++ code/06_gdiv.sh
@@ -21,80 +21,75 @@ git pull
 # git clone https://github.com/mhelmkampf/meg26.git
 
 
-### Prepare today's working directory
-cd work/snps
-# move all log files into "logs" subdirectory
+### Create and navigate to today's working directory (e.g. "gdiv")
 #>
 
 
 
 ### ============================================================================
-### Exercise 1:
+### Exercise 1: Heterozygosity and inbreeding coefficient at variant sites
 
 module load BCFtools
 module load VCFtools
 
 
-### Create link to today's SNP dataset (only if file does not exist already)
-# ln -s /nfs/data/haex1482/shared/course/hamlets_filt_lg12.vcf.gz hamlets_filt_lg12.vcf.gz
+### Create link to today's SNP dataset
+ln -s /nfs/data/haex1482/shared/course/hamlets_snps_lg12.vcf.gz \
+  hamlets_snps_lg12.vcf.gz
 
 
-### Copy population id files to working directory
-cp ../../meg25/data/meta/*.txt .
+### Filter SNPs without dropping rare alleles (--maf)
+vcftools \
+  --gzvcf hamlets_snps_lg12.vcf.gz \
+  --minQ 40 \
+  --minDP 10 \
+  --max-missing 0.8 \
+  --max-alleles 2 \
+  --recode \
+  --stdout | bgzip > hamlets_bi_lg12.vcf.gz
 
 
 ### Calculate heterozygosity and Fis per individual
 vcftools \
-  --gzvcf hamlets_filt_lg12.vcf.gz \
+  --gzvcf hamlets_bi_lg12.vcf.gz \
+  --het \
+  --stdout > Het_hamlets.tsv
+
+
+### What is unusual about these Fis values, and why?
+#>
+
+
+### Copy population id files to working directory
+cp ../../data/meta/*.txt .
+
+
+### Re-calculate heterozygosity for puebel sub-population only
+vcftools \
+  --gzvcf hamlets_bi_lg12.vcf.gz \
   --keep hamlet_puebel.txt \
   --het \
   --stdout > Het_puebel.tsv
 
-vcftools \
-  --gzvcf hamlets_filt_lg12.vcf.gz \
-  --keep hamlet_atlhua.txt \
-  --het \
-  --stdout > Het_atlhua.tsv
 
-column -t Het_puebel.tsv
+### Re-calculate heterozygosity for atlahua sub-population (hamlet_atlhua.txt) only
+#>
 
-### From here, we could average and plot Fis per population
-
-
-ln -s /nfs/data/haex1482/shared/course/guitar_filt_10ctg.vcf.gz guitar_filt_10ctg.vcf.gz
-
-### Calculate heterozygosity and Fis per individual
-
-vcftools \
-  --gzvcf guitar_filt_10ctg.vcf.gz \
-  --keep guitar_spain.txt \
-  --het \
-  --stdout > Het_spain.tsv
-
-vcftools \
-  --gzvcf guitar_filt_10ctg.vcf.gz \
-  --keep guitar_cabov.txt \
-  --het \
-  --stdout > Het_cabov.tsv
 
 
 ### ============================================================================
-### Exercise 2: Assess genetic diversity (nucleotide diversity)
+### Exercise 2: Assess nucleotide diversity per population
 
-### To calculate nucleotide diversity, we need all sites, not just SNPs:
+### To calculate nucleotide diversity, we need variant AND invariant sites
 ln -s /nfs/data/haex1482/shared/course/hamlets_all_1M.vcf.gz hamlets_all_1M.vcf.gz
 
 
-### Copy population id files to working directory
-cp ../../meg25/data/gdiv/*.txt .
-
-
-### Measure nucleotide divergence per species / population
-for i in pop_*.txt; do
+### Measure nucleotide diversity per population using shell loop
+for i in hamlet_*.txt; do
 vcftools \
-  --gzvcf hamlets_LG12-1M_all.vcf.gz \
+  --gzvcf hamlets_all_1M.vcf.gz \
   --keep "$i" \
-  --site-pi \
+  --window-pi 50000 \
   --stdout > Pi_"$i".tsv
 done
 
@@ -102,7 +97,7 @@ done
 ### Average pi per population
 for i in Pi_*.tsv; do
   echo $i
-  awk '$3 != "-nan" { sum += $3 ; next } END { print sum / NR }' $i
+  awk '$5 != "-nan" { sum += $5 ; next } END { print sum / NR }' $i
   echo
 done
 
@@ -112,95 +107,64 @@ done
 ### Exercise 3: Filter by linkage disequilibrium
 
 ### Referring to the linkage decay plot, how many basepairs (bp) apart should the 
-### sites be from each other to avoid linkage disequilibrium? Use vcftools --thin
-### to create such a dataset
+### sites be from each other to avoid linkage disequilibrium?
 
 
-### Calculate the correlation coefficient of linkage disequilibrium in the first
-### 100000 bp of both datasets (before and after thinning)
-vcftools --gzvcf hamlets_LG12_snp.vcf.gz \
-  --hap-r2 \
-  --stdout > R2_before_thinning.tsv
-
-vcftools --gzvcf hamlets_LG12_snp_2kb.vcf.gz \
-  --hap-r2 \
-  --stdout > R2_after_thinning.tsv
+### Remove rare alleles for LD analyses
+vcftools --gzvcf hamlets_bi_lg12.vcf.gz \
+  --maf 0.05 \
+  --max-missing 0.9 \
+  --recode \
+  --stdout | bgzip > hamlets_maf_lg12.vcf.gz
 
 
-### Plot R2 distribution before and after thinning, in ASCII
-for file in R2_before_thinning.tsv R2_after_thinning.tsv; do
-  echo
-  echo "==> $file <=="
-  awk '
-  BEGIN { bin_size=0.05 }
-  /^CHR/ { next }  # skip header
-  {
-    if ($5 != "-nan") {
-      r2 = $5 + 0
-      bin = int(r2 / bin_size)
-      counts[bin]++
-      total++
-    }
-  }
-  END {
-    max_percent = 0
-    for (i in counts) {
-      percent = 100 * counts[i] / total
-      if (percent > max_percent) max_percent = percent
-    }
-    for (i = 0; i <= int(1 / bin_size); i++) {
-      percent = 100 * counts[i] / total
-      bar_len = int((percent / max_percent) * 50)
-      bin_label = sprintf("%.2f", i * bin_size)
-      printf "%5s | %s (%.1f%%)\n", bin_label, substr("##################################################", 1, bar_len), percent
-    }
-  }' "$file"
-  echo
-done
+### Calculate R^2 without thinning
+vcftools --gzvcf hamlets_maf_lg12.vcf.gz \
+  --geno-r2 \
+  --ld-window-bp 10000 \
+  --out ld_unthinned
+
+
+### Thin to 1 SNP per 5000 bp
+vcftools --gzvcf hamlets_maf_lg12.vcf.gz \
+  --thin 5000 \
+  --recode \
+  --stdout | bgzip > hamlets_thin_lg12.vcf.gz
+
+
+### Calculate R^2 after thinning
+vcftools --gzvcf hamlets_thin_lg12.vcf.gz \
+  --geno-r2 \
+  --ld-window-bp 10000 \
+  --out ld_thinned
+
+
+### Calculate mean R^2
+# Unthinned
+awk 'NR>1 && $5!="-nan" {sum+=$5; n++} END {print "mean r2 =", sum/n}' \
+  ld_thinned.geno.ld
+  
+awk 'NR>1 && $5!="-nan" {sum+=$5; n++} END {print "mean r2 =", sum/n}' \
+  ld_unthinned.geno.ld
 
 
 
 ### ============================================================================
 ### Solutions:
 
-### VCF stats
-bcftools stats hamlets_LG12_snp.vcf.gz | grep 'SN'
-#> 1032804 sites
-
-zcat hamlets_LG12_snp.vcf.gz | grep -c -v '#'
-
-
-### Include only sites with minor allele count of at least 2
+### Re-calculate heterozygosity for atlahua sub-population only
 vcftools \
-    --gzvcf hamlets_LG12_snp.vcf.gz \
-    --mac 2 \
-    --recode \
-    --stdout | bgzip > hamlets_LG12_snp_mac2.vcf.gz
-
-
-### Summarize
-bcftools stats hamlets_LG12_snp_mac2.vcf.gz | grep 'SN'
-#> 319960 sites
-
-
-### Spot check
-zcat hamlets_LG12_snp_mac2.vcf.gz | tail
-
-
-### Thin the filtered data so that no two sites are within 2000 bp from each other
-vcftools \
-    --gzvcf hamlets_LG12_snp.vcf.gz \
-    --thin 2000 \
-    --recode \
-    --stdout | bgzip > hamlets_LG12_snp_2kb.vcf.gz
-
+  --gzvcf hamlets_bi_lg12.vcf.gz \
+  --keep hamlet_atlhua.txt \
+  --het \
+  --stdout > Het_atlhua.tsv
 
 
 ### ============================================================================
 ### Alternative code and notes
 
 ### Summarize in ASCII
-for file in R2_before_thinning.tsv R2_after_thinning.tsv; do
+for file in ld_thinned.geno.ld ld_unthinned.geno.ld; do
   echo
   echo "$file:"
   awk '$5 != "-nan" { sum += $5; n++; vals[n] = $5 }
